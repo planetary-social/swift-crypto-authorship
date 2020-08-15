96eb88f119c05c653168dadd0ef228ad398a552d
月光虹 <iris@random.town>
2020-08-15 18:18:51 +0200

    Add the Identification Props package...
    ... together with Elliptic Curve Identification Tests package.

--- Package.swift
+++ Package.swift
@@ -15,6 +15,9 @@ let package = Package(
             name: "Identify",
             targets: ["Identify"]),
         .library(
+            name: "IdentificationProps",
+            targets: ["IdentificationProps"]),
+        .library(
             name: "EllipticCurveIdentification",
             targets: ["EllipticCurveIdentification"]),
     ],
@@ -27,11 +30,23 @@ let package = Package(
             name: "Identify",
             dependencies: []),
         .target(
+            name: "IdentificationProps",
+            dependencies: [
+                "Identify",
+            ]),
+        .target(
             name: "EllipticCurveIdentification",
             dependencies: [
                 "Identify",
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "Logging", package: "swift-log"),
+            ]),
+        .testTarget(
+            name: "EllipticCurveIdentificationTests",
+            dependencies: [
+                "Identify",
+                "IdentificationProps",
+                "EllipticCurveIdentification",
             ]),
     ]
 )
