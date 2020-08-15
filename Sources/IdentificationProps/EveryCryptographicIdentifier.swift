008dcbba493f4cda8b605219ddc97b20f4aec8dd
月光虹 <iris@random.town>
2020-08-15 18:20:58 +0200

    Define common properties of every Cryptographic Identifier
    Crypto Identity and Authenticity Identifier should both fulfil the role of a Cryptographic Identifier.

---
+++ Sources/IdentificationProps/EveryCryptographicIdentifier.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+@testable import Identify
+
+extension UniversalProperties {
+
+    /// Is this a cryptographic identifier (either an identity or public identifier)?
+    ///
+    /// Implementations of `CryptoIdentity` and  `AuthenticityIdentifier` should satisfy the following properties:
+    ///
+    /// - fixed-length;
+    /// - conformant to `RawRepresentable`.
+    ///
+    
+    struct EveryCryptographicIdentifier<Implementation: RawRepresentable>
+    where Implementation.RawValue == Data {
+
+        /// A cryptographic identifier to be tested against these properties.
+        
+        let identifier: Implementation
+        
+        /// Expected size of the fixed-length key.
+        
+        let exactKeyByteCount: Int
+
+        /// Key should be fixed-length.
+        
+        func alwaysTheSameExactByteCount() -> Bool {
+            return identifier.rawValue.count == exactKeyByteCount
+        }
+        
+        /// Cryptographic identifier can be represented as raw byte-string stored in `Data` object.
+        
+        func correctlyConformsToRawRepresentable() -> Bool {
+            let tamperedSize = exactKeyByteCount - Int.random(in: 1...exactKeyByteCount)
+
+            return
+                Implementation(rawValue: identifier.rawValue.prefix(tamperedSize)) == nil &&
+                Implementation(rawValue: identifier.rawValue)?.rawValue == identifier.rawValue
+        }
+
+    }
+
+}
